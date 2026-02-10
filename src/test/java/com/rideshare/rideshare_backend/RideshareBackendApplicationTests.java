c8f9c9ac1ac89f0a8a496043f2a7c20280b38d7f
Greattony Oduche <greattoy96@gmail.com>
2026-02-10 14:27:07 +0300

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/rideshare/rideshare_backend/RideshareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rideshare.rideshare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideshareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
